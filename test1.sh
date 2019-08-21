0205e6dad8ddf2dcc893e18d822fcd24a20f678e
Regler <1263826590@qq.com>
2019-08-21 16:17:23 +0800

    修改第一题

--- test1.sh
+++ test1.sh
@@ -1,21 +1,40 @@
 #!/bin/bash	
-	read -p "请输字符串  " str1
+flag=0
+echo " lower:转化为小写  upper:转化为大写 quit和exit:退出交互程序 "
 	while :
 	do
-	read -p "请输入命令 " cmd 
-		if [ $cmd == "lower" ]
-			then 
-			echo  $str1 | tr "[:upper:]" "[:lower:]" 
-		elif [ $cmd == "upper" ]
-			then 
-			 echo $str1 | tr "[:lower:]" "[:upper:]" 
-		elif [ $cmd == "exit" -o $cmd == "quit" ]
-			then 
+	read  -p "请输入字符或者命令: "  cmd
+		if [ "$cmd" == "lower" ]
+		then
+			flag=1		
+			echo "切换为转化小写模式"
+		elif [ "$cmd" == "upper" ]
+		then 
+			flag=2
+			echo "切换为转化大写模式"
+		elif [ "$cmd" == "exit" -o "$cmd" == "quit" ]
+		then 
 			exit
-		else 
-			sum=0
-			echo $str1 | tr "[:upper:]" "[:lower:]" 
-			echo $str1 | awk '{ for(i=1;i<=NF;i++)  sum +=length($i) } END{print sum}'
 		fi
+	
+
+		if [ $flag == 0 ]
+		then
+			sum=0
+			echo "$cmd" | tr "[:upper:]" "[:lower:]" 
+			echo "$cmd" | awk '{ for(i=1;i<=NF;i++)  sum +=length($i) } END{print sum}'
+		elif [ $flag == 1 ]
+		then
+			if [[  "$cmd" != "lower" && "$cmd" != "upper"  ]]
+			then
+				echo  "$cmd" | tr "[:upper:]" "[:lower:]" 
+			fi
+		else 
+			if [[  "$cmd" != "lower" &&  "$cmd" != "upper"  ]]
+			then
+				echo "$cmd"  | tr "[:lower:]" "[:upper:]" 
+			fi
+		fi
+
 	done
 
